4e8a2f2dcd6a2abb89ef6280d2310c4af6c0c291
Rosangela Alecrim <rosagela.melo@prodam.am.gov.br>
2017-08-01 10:42:43 -0400

    Atualização do script do banco de dados

--- helpDesk-scripts/helpdesk.sql
+++ helpDesk-scripts/helpdesk.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `helpdesk` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `helpdesk`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
 --
 -- Host: 127.0.0.1    Database: helpdesk
@@ -16,6 +18,195 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `arquivo_chamado`
+--
+
+DROP TABLE IF EXISTS `arquivo_chamado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `arquivo_chamado` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `FOTO` blob,
+  `ID_CHAMADO` bigint(20) NOT NULL,
+  `VIDEO` longblob,
+  PRIMARY KEY (`ID`),
+  KEY `FK_FOTO_CHAMADO_idx` (`ID_CHAMADO`),
+  CONSTRAINT `FK_FOTO_CHAMADO` FOREIGN KEY (`ID_CHAMADO`) REFERENCES `chamado` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `arquivo_chamado`
+--
+
+LOCK TABLES `arquivo_chamado` WRITE;
+/*!40000 ALTER TABLE `arquivo_chamado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `arquivo_chamado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookmark`
+--
+
+DROP TABLE IF EXISTS `bookmark`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bookmark` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookmark`
+--
+
+LOCK TABLES `bookmark` WRITE;
+/*!40000 ALTER TABLE `bookmark` DISABLE KEYS */;
+INSERT INTO `bookmark` VALUES (1,'Portal','http://www.frameworkdemoiselle.gov.br'),(2,'Documentação','http://demoiselle.sourceforge.net/docs/framework/reference'),(3,'Fórum','http://pt.stackoverflow.com/tags/demoiselle'),(4,'Lista de usuários','https://lists.sourceforge.net/lists/listinfo/demoiselle-users'),(5,'Blog oficial','http://frameworkdemoiselle.wordpress.com'),(6,'Blog experimental','http://demoisellelab.wordpress.com'),(7,'Repositório','http://github.com/demoiselle/framework'),(8,'Bug Tracker','https://demoiselle.atlassian.net'),(9,'Facebook','http://facebook.com/FrameworkDemoiselle'),(10,'Twitter','http://twitter.com/fwkdemoiselle'),(11,'Distribuição','http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22br.gov.frameworkdemoiselle%22'),(12,'Binários','http://sourceforge.net/projects/demoiselle/files/framework');
+/*!40000 ALTER TABLE `bookmark` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `ID` bigint(20) NOT NULL,
+  `NOME` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chamado`
+--
+
+DROP TABLE IF EXISTS `chamado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chamado` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `DESCRICAO` varchar(1000) DEFAULT NULL,
+  `DATA_CRIACAO` datetime DEFAULT NULL,
+  `ID_USUARIO` bigint(20) NOT NULL,
+  `ID_CATEGORIA` bigint(20) NOT NULL,
+  `ID_STATUS` bigint(20) NOT NULL,
+  `DATA_CRIACAO` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_CHAMADO_USUARIO_idx` (`ID_USUARIO`),
+  KEY `FK_CHAMADA_CATEGORIA_idx` (`ID_CATEGORIA`),
+  KEY `FK_CHAMADO_STATUS_idx` (`ID_STATUS`),
+  CONSTRAINT `FK_CHAMADO_CATEGORIA` FOREIGN KEY (`ID_CATEGORIA`) REFERENCES `categoria` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_CHAMADO_STATUS` FOREIGN KEY (`ID_STATUS`) REFERENCES `status` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_CHAMADO_USUARIO` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chamado`
+--
+
+LOCK TABLES `chamado` WRITE;
+/*!40000 ALTER TABLE `chamado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chamado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comentario`
+--
+
+DROP TABLE IF EXISTS `comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comentario` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `DATA_COMENTARIO` datetime DEFAULT NULL,
+  `DESCRICAO` varchar(1000) DEFAULT NULL,
+  `ID_CHAMADO` bigint(20) NOT NULL,
+  `ID_USUARIO` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_COMENTARIO_CHAMADO_idx` (`ID_CHAMADO`),
+  KEY `FK_COMENTARIO_USUARIO_idx` (`ID_USUARIO`),
+  CONSTRAINT `FK_COMENTARIO_CHAMADO` FOREIGN KEY (`ID_CHAMADO`) REFERENCES `chamado` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_COMENTARIO_USUARIO` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comentario`
+--
+
+LOCK TABLES `comentario` WRITE;
+/*!40000 ALTER TABLE `comentario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comentario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `foto_usuario`
+--
+
+DROP TABLE IF EXISTS `foto_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `foto_usuario` (
+  `ID` bigint(20) NOT NULL,
+  `ID_USUARIO` bigint(20) DEFAULT NULL,
+  `FOTO` blob,
+  PRIMARY KEY (`ID`),
+  KEY `FK_FOTO_USUARIO_idx` (`ID_USUARIO`),
+  CONSTRAINT `FK_FOTO_USUARIO` FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `foto_usuario`
+--
+
+LOCK TABLES `foto_usuario` WRITE;
+/*!40000 ALTER TABLE `foto_usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `foto_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `status` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `SIGLA` char(1) DEFAULT NULL,
+  `DESCRICAO` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `usuario`
 --
 
@@ -23,14 +214,14 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `usuId` int(11) NOT NULL AUTO_INCREMENT,
-  `usuNome` varchar(20) NOT NULL,
-  `usuSobrenome` varchar(20) NOT NULL,
-  `usuEmail` varchar(45) NOT NULL,
-  `usuNascimento` date NOT NULL,
-  `usuSexo` varchar(45) NOT NULL,
-  PRIMARY KEY (`usuId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `EMAIL` varchar(255) DEFAULT NULL,
+  `NOME` varchar(255) DEFAULT NULL,
+  `SEXO` varchar(255) DEFAULT NULL,
+  `SOBRENOME` varchar(255) DEFAULT NULL,
+  `SENHA` varchar(10) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +230,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'rosangela.alecrim2013@gmail.com','rosangela',NULL,'alecrim','1'),(2,'usuario@gmail.com','usuario',NULL,'alecrim','1'),(3,'fulano@gmail.com','fulano',NULL,'alecrim','1');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +243,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-14 17:42:32
+-- Dump completed on 2017-08-01 10:41:13
