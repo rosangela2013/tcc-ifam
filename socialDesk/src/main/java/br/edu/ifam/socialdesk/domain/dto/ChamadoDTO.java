11872405e6a0a0920cf1e9312c844efd3ebcc1f7
Rosangela Alecrim <rosagela.melo@prodam.am.gov.br>
2017-08-11 14:26:42 -0400

    implementacao arquivoChamado

--- socialDesk/src/main/java/br/edu/ifam/socialdesk/domain/dto/ChamadoDTO.java
+++ socialDesk/src/main/java/br/edu/ifam/socialdesk/domain/dto/ChamadoDTO.java
@@ -1,44 +1,59 @@
 package br.edu.ifam.socialdesk.domain.dto;
 
+import javax.validation.constraints.NotNull;
+
+import br.edu.ifam.socialdesk.domain.Categoria;
 import br.edu.ifam.socialdesk.domain.Chamado;
+import br.edu.ifam.socialdesk.domain.Usuario;
 
-public class ChamadoDTO {
+public class CriarChamadoDTO {
 
-	private Chamado chamado;
-	private Long qtdComentarios;
-	private String urlArquivo;
+	@NotNull
+	private String descricao;
 
-	public ChamadoDTO() {
+	@NotNull
+	private Long idUsuario;
+	@NotNull
+	private Long idCategoria;
+
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public ChamadoDTO(Chamado chamado, Long qtdComentarios, String urlArquivo) {
-		super();
-		this.chamado = chamado;
-		this.qtdComentarios = qtdComentarios;
-		this.urlArquivo = urlArquivo;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
 	}
 
 	public Chamado getChamado() {
+		Chamado chamado = new Chamado();
+		chamado.setDescricao(getDescricao());
+
+		if (this.idCategoria != null) {
+			chamado.setCategoria(new Categoria());
+			chamado.getCategoria().setId(idCategoria);
+		}
+
+		if (this.idUsuario != null) {
+			chamado.setUsuario(new Usuario());
+			chamado.getUsuario().setId(idUsuario);
+		}
 		return chamado;
 	}
 
-	public void setChamado(Chamado chamado) {
-		this.chamado = chamado;
-	}
-
-	public Long getQtdComentarios() {
-		return qtdComentarios;
-	}
-
-	public void setQtdComentarios(Long qtdComentarios) {
-		this.qtdComentarios = qtdComentarios;
-	}
-
-	public String getUrlArquivo() {
-		return urlArquivo;
-	}
-
-	public void setUrlArquivo(String urlArquivo) {
-		this.urlArquivo = urlArquivo;
-	}
 }
